1b9398fbd0decf6f56df6a6d9f09f2c3a7725a98
Eric Chung <ericchung@gmail.com>
2024-03-19 13:59:45 +0800

    trying to fix spacing

--- views/tes.py
+++ views/tes.py
@@ -8,26 +8,25 @@ import re
 def pdf_to_images(pdf_path):
     return convert_from_path(pdf_path)
 
-def images_to_text(images):
+def images_to_text(images, doc_path):
+    doc = Document()
     # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     pytesseract.pytesseract.tesseract_cmd = r'/app/.apt/usr/bin/tesseract'
     text = ""
+    custom_config = '--oem 3 --psm 6'
     for i, img in enumerate(images):
-        img_text = pytesseract.image_to_string(img)
+        img_text = pytesseract.image_to_string(img, config=custom_config)
         img_text = re.sub(r'[\x00-\x1F\x7F]', '', img_text)
-        text += img_text
+        text_to_word(img_text, doc)
         print(f"{(i + 1) / len(images) * 100}%")  # print progress
-    return text
-
-def text_to_word(text, doc_path):
-    doc = Document()
-    doc.add_paragraph(text)
     doc.save(doc_path)
 
+def text_to_word(text, doc):
+    doc.add_paragraph(text)
+    
 
 pdf_path = sys.argv[1]
 doc_path = sys.argv[2]
 
 images = pdf_to_images(pdf_path)
-text = images_to_text(images)
-text_to_word(text, doc_path)
+text = images_to_text(images, doc_path)
